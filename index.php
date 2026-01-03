cac3fbedf99852ef388e4e2edaefd9653bd4d3ee
AnnriSu <anneritchel.deguzman.sumague@gmail.com>
2026-01-03 21:02:45 +0800

    landing page

---
+++ index.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+
+    <link href="assets\style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <!-- Navigation Bar -->
+    <nav class="navbar shadow-sm">
+
+        <div class="container-fluid sticky-top">
+
+            <div class="d-flex gap-4 me-4">
+                <a class="navbar-brand d-flex ms-4" href="index.html">
+                    <img src="assets\img\isc_brand_bold.png" alt="Logo" width="250"
+                        height="auto" class="mt-1 mb-1">
+                </a>
+            </div>
+
+            <div class="d-flex gap-4 me-4">
+                <button class="btn rounded rounded-5  pb-2 pt-2 px-4">
+                    <a class="nav-link text-light fw-bold" href="login.php">
+                        <h5>Login</h5>
+                    </a>
+                </button>
+
+                <button class="btn rounded rounded-5  pb-2 pt-2 px-4">
+                    <a class="nav-link text-light fw-bold" href="login.php">
+                        <h5>Apply</h5>
+                    </a>
+                </button>
+            </div>
+        </div>
+    </nav>
+
+
+
+
+
+    <!-- Apply Now Container -->
+
+
+    <div class="container-fluid introduction-container p-0 shadow-sm pb-0">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 text-dark align-self-center">
+                <div class="p-4 m-2 text-lg-start text-center">
+                    <h1 class="fw-bold">Join the Iskonnovators Student
+                        Community!</h1>
+                    <p>Become a part of our vibrant community and unlock a world of opportunities. By joining the ISC, you'll connect with others, grow your skills, and take part in meaningful experiences. Apply now and embark on an exciting journey with the ISC.</p>
+
+                    <button class="btn btn-primary btn-lg rounded rounded-5 px-5 pb-2 pt-2">
+                        <a class="nav-link text-light" href="login.php">
+                            <h4 class="fw-bold">Apply Now!</h4>
+                        </a>
+                    </button>
+
+                </div>
+            </div>
+            
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+                <img src="assets/img/mascot.png" alt="Banner" class="img-fluid d-none d-lg-block" style="object-fit:contain;">
+                <img src="assets/img/mascot full.png" alt="Banner" class="img-fluid d-lg-none" style="object-fit:contain;">
+                
+            </div>
+        </div>
+
+    </div>
+
+
+
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
