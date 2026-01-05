1b6518a50a8dba6bdecd568a32a2f51dfd28e041
AnnriSu <anneritchel.deguzman.sumague@gmail.com>
2026-01-05 11:58:00 +0800

    started on apply.php

---
+++ apply.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Iskonnovators Student Community</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link href="assets/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <!-- Navigation Bar -->
+    <nav class="navbar shadow-sm">
+
+        <div class="container-fluid sticky-top">
+
+            <div class="d-flex gap-4 me-4">
+                <a class="navbar-brand d-flex ms-4" href="index.php">
+                    <img src="assets\img\isc_brand_bold.png" alt="Logo" width="250"
+                        height="auto" class="mt-1 mb-1">
+                </a>
+            </div>
+
+            <div class="d-flex gap-4 me-4">
+                <button class="btn rounded rounded-5  pb-2 pt-2 px-4">
+                    <a class="nav-link text-light fw-bold" href="login.php">
+                        <h5>Login</h5>
+                    </a>
+                </button>
+
+                <button class="btn rounded rounded-5  pb-2 pt-2 px-4">
+                    <a class="nav-link text-light fw-bold" href="apply.php">
+                        <h5>Apply</h5>
+                    </a>
+                </button>
+            </div>
+        </div>
+    </nav>
+
+
+   
+
+
+    <!-- footer -->
+    <footer class="footer text-center text-lg-start mt-4 fixed-bottom ">
+        <div class="text-center p-3">
+            © 2025 Iskonnovators Student Community PUPSTC, All Rights Reserved
+        </div>
+    </footer>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         <div class="container-fluid sticky-top">
 
             <div class="d-flex gap-4 me-4">
-                <a class="navbar-brand d-flex ms-4" href="index.html">
+                <a class="navbar-brand d-flex ms-4" href="index.php">
                     <img src="assets\img\isc_brand_bold.png" alt="Logo" width="250"
                         height="auto" class="mt-1 mb-1">
                 </a>
@@ -51,7 +51,7 @@
                     <p>Become a part of our vibrant community and unlock a world of opportunities. By joining the ISC, you'll connect with others, grow your skills, and take part in meaningful experiences. Apply now and embark on an exciting journey with the ISC.</p>
 
                     <button class="btn btn-primary btn-lg rounded rounded-5 px-5 pb-2 pt-2">
-                        <a class="nav-link text-light" href="login.php">
+                        <a class="nav-link text-light" href="apply.php">
                             <h4 class="fw-bold">Apply Now!</h4>
                         </a>
                     </button>
@@ -70,7 +70,7 @@
 
 
     <!-- footer -->
-    <footer class="footer text-center text-lg-start mt-4">
+    <footer class="footer text-center text-lg-start mt-4 fixed-bottom ">
         <div class="text-center p-3">
             © 2025 Iskonnovators Student Community PUPSTC, All Rights Reserved
         </div>
