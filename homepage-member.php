4b655db0126cfb04b019d013cd3d1f860e48122b
Akhicia Mae Torreliza <calangiakhiciamae@gmail.com>
2026-01-06 02:45:08 +0800

    Template for homepage-member inactive

---
+++ homepage-member.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Iskonnovators Student Community</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link href="assets/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <nav class="navbar shadow-sm">
+
+        <div class="container-fluid sticky-top">
+
+            <div class="d-flex gap-4 me-4">
+                <a class="navbar-brand d-flex ms-4" href="index.php">
+                    <img src="assets\img\isc_brand_bold.png" alt="Logo" width="250" height="auto" class="mt-1 mb-1">
+                </a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container-fluid mt-4">
+        <div class="row g-4">
+
+            <div class="col-md-3 col-lg-2">
+                <div class="bg-primary text-white rounded-4 p-3 shadow" style="min-height:450px;">
+
+                    <h5 class="text-center fw-bold mb-4">Member</h5>
+
+                    <div class="text-center mb-3">
+                        <div class="rounded-circle bg-white text-primary d-flex
+                                align-items-center justify-content-center mx-auto mb-2"
+                            style="width:80px; height:80px; font-size:40px;">
+                            👤
+                        </div>
+                        <small>Juan Dela Cruz</small>
+                    </div>
+
+                    <div class="d-grid gap-2">
+                    <button class=" btn btn-sidebar">Events</button>
+                        <button class="btn btn-sidebar">Newsletters</button>
+                        <button class="btn btn-sidebar">Report</button>
+                        <button class="btn btn-sidebar">Edit Profile</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-9 col-lg-10">
+                <div class="bg-white rounded-4 p-4 shadow" style="border:3px solid #668cd8ff; min-height:450px;">
+
+                    <h5 class="fw-bold mb-0">Home</h5>
+                    <small class="text-muted">Dashboard</small>
+
+                    <!-- EVENT CARD TEMPLATE -->
+                    <div class="border rounded-3 p-3 mt-4 d-flex gap-3 align-items-start">
+
+                        <div class="bg-light rounded d-flex align-items-center justify-content-center"
+                            style="width:120px; height:120px;">
+                            <span class="text-muted">Image</span>
+                        </div>
+
+                        <!-- Idouble na lang to -->
+                        <div class="flex-grow-1">
+                            <h6 class="fw-bold mb-1">Event Name</h6>
+                            <small class="d-block">Description</small>
+                            <small class="d-block">Date & Time</small>
+                            <small class="d-block">Instructor</small>
+
+                            <button class="btn btn-primary btn-sm mt-2">Button</button>
+                        </div>
+                    </div>
+                    <!-- END OF EVENT CARD TEMPLATE -->
+
+                </div>
+            </div>
+
+
+            <!-- footer -->
+            <footer class="footer text-center text-lg-start mt-4 fixed-bottom ">
+                <div class="text-center p-3">
+                    © 2025 Iskonnovators Student Community PUPSTC, All Rights Reserved
+                </div>
+            </footer>
+
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+                crossorigin="anonymous"></script>
+</body>
+
+</html>
