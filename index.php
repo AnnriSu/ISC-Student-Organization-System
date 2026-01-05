1038db8d2407a579957e926cf4efbbfc2bca4d9e
AnnriSu <anneritchel.deguzman.sumague@gmail.com>
2026-01-05 11:50:44 +0800

    landing page footer

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Iskonnovators Student Community</title>
 
-    <link href="assets\style.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link href="assets/style.css" rel="stylesheet">
 </head>
 
 <body>
@@ -32,7 +32,7 @@
                 </button>
 
                 <button class="btn rounded rounded-5  pb-2 pt-2 px-4">
-                    <a class="nav-link text-light fw-bold" href="login.php">
+                    <a class="nav-link text-light fw-bold" href="apply.php">
                         <h5>Apply</h5>
                     </a>
                 </button>
@@ -58,24 +58,23 @@
 
                 </div>
             </div>
-            
+
             <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
                 <img src="assets/img/mascot.png" alt="Banner" class="img-fluid d-none d-lg-block" style="object-fit:contain;">
                 <img src="assets/img/mascot full.png" alt="Banner" class="img-fluid d-lg-none" style="object-fit:contain;">
-                
+
             </div>
         </div>
 
     </div>
 
+
     <!-- footer -->
-     <footer class="text-center text-lg-start mt-4">
+    <footer class="footer text-center text-lg-start mt-4">
         <div class="text-center p-3">
             © 2025 Iskonnovators Student Community PUPSTC, All Rights Reserved
         </div>
     </footer>
-
-    </div>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
