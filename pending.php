7089c4cd53afa0672f63344bd4df126cf5e56349
AnnriSu <anneritchel.deguzman.sumague@gmail.com>
2026-01-06 10:50:26 +0800

    added pending page

--- pending.php
+++ pending.php
@@ -26,15 +26,40 @@
         </div>
     </nav>
 
-    <div class="container form-container mt-3 mb-3 p-4 shadow-sm rounded-3 " style="max-width: 700px;">
+    <div class="container form-container mt-5 mb-5 p-4 shadow-sm rounded-3 " style="max-width: 500px;">
         
+        <div class="mx-auto mb-3 d-flex align-items-center justify-content-center rounded icon-box">
+            <img src="assets\img\mail.png" alt="Application Received" width="150" height="auto">
+        </div>
+
+        <hr>
+
+        <h3 class="fw-bold mb-3 d-flex align-items-center justify-content-center text-center">Membership Application Received</h3>
+
+        <p class="mb-2 px-2 d-flex align-items-center justify-content-center text-center">
+            We have successfully received your membership application.
+            Our team is currently reviewing your submission.
+            <br><br>
+            Please wait for a reply email from the organization regarding the status of your application.
+            Further instructions will be sent once the review process is complete.
+        </p>
+
+        <p class="fw-semibold d-flex align-items-center justify-content-center">Thank you for your interest and patience.</p>
+
+        <hr>
+
+        <p class="text-muted text-muted-small mb-0 d-flex align-items-center justify-content-center text-center">
+            If you do not receive a reply within a few days, please contact the organization for assistance.
+        </p>
 
     </div>
+
+    
 
 
 
     <!-- footer -->
-    <footer class="footer text-center text-lg-start mt-4 fixed-bottom ">
+    <footer class="footer text-center text-lg-start mt-4  ">
         <div class="text-center p-3">
             © 2025 Iskonnovators Student Community PUPSTC, All Rights Reserved
         </div>
