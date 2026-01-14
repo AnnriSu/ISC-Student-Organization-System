7c1ce626846bfe37bac95ab06844e1708f6f2089
Chelle <rochelledvlsc@gmail.com>
2026-01-14 17:22:31 +0800

    removed logout button

--- sponsorship.php
+++ sponsorship.php
@@ -273,13 +273,7 @@ function saveSponsorToDatabase(string $sponsorName): int
                     <img src="assets\img\isc_brand_bold.png" alt="Logo" width="250" height="auto" class="mt-1 mb-1">
                 </a>
             </div>
-
-            <div
-                class="pe-sm-3 d-flex flex-column flex-sm-row gap-2 gap-lg-4 align-items-center justify-content-center justify-content-md-end ms-md-auto">
-                <a class="navbar-brand d-flex ms-4" href="index.php">
-                    <img src="assets\img\Log out.svg" alt="Logo" width="30" height="auto" class="mt-1 mb-1">
-                </a>
-            </div>
+            
         </div>
     </nav>
 
